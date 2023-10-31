4a18eec97bf34104e339d0200e8198e1ccd15716
Matt Etress <mattetress@gmail.com>
2023-10-31 11:53:09 -0500

    update api url

--- public/graphics.js
+++ public/graphics.js
@@ -216,7 +216,7 @@ function postToAPI(after) {
     }
   };
 
-  fetch("https://koan-tool.onrender.com/api/v1/boards", {
+  fetch("/api/v1/boards", {
     method: "post",
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
@@ -250,7 +250,7 @@ $(document).ready(function() {
   $('form').on('submit', function(e){
     e.preventDefault();
     let id = document.getElementById('loadId').value;
-    let url = "https://koan-tool.onrender.com/api/v1/boards/" + id;
+    let url = "/api/v1/boards/" + id;
 
     document.getElementById('loadId').value = '';
 
